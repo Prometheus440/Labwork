be73d380025a394a8c096350c2260b6273f34b15
Matt Gilks <matthewgilks05@icloud.com>
2025-05-15 12:00:22 +0100

    Create shape.h

---
+++ Shape classes/shape.h
@@ -0,0 +1,20 @@
+//Declare shape class
+#ifndef SHAPE_H
+#define SHAPE_H
+
+class Shape
+{
+	//will have 2 properties
+	//height and width
+protected:
+	int iHeight;
+	int iWidth;
+
+	void calculateArea(int iHeightValue, int iWidthValue);
+
+	void setDimensions(int iHeightValue, int iWidthValue);
+	int getHeight(int iHeightValue);
+	int getWidth(int iWidthValue);
+};
+
+#endif // SHAPE_H
