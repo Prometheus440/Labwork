3fe620b0faede851cf166ef17e1b6446acb4ecfd
Matt Gilks <matthewgilks05@icloud.com>
2025-05-15 12:00:39 +0100

    Create shape.cpp

---
+++ Shape classes/shape.cpp
@@ -0,0 +1,22 @@
+//Define shape class
+#include "shape.h"
+
+void Shape::calculateArea(int iHeightValue, int IWidthValue)
+{
+}
+
+void Shape::setDimensions(int iHeightValue, int iWidthValue)
+{
+	iHeight = iHeightValue;
+	iWidth = iWidthValue;
+}
+
+int Shape::getHeight(int iHeightValue)
+{
+	return iHeight;
+}
+
+int Shape::getWidth(int iWidthValue)
+{
+	return iWidth;
+}
