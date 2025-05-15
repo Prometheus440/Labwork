afee9eb82deff9174f20fc749bc577605f8d590b
Matt Gilks <matthewgilks05@icloud.com>
2025-05-15 11:58:50 +0100

    Create main.cpp

---
+++ Shape classes/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include "shape.h"
+
+/*
+    Defines shape class with constructors and properties (width and height)
+    Implement methods that calculate the area of the shape (overwrite the method like speak())
+    Get and set the height and width properties
+    Define 3 derived classes - square, rectangle and triangle that inherit the shape class
+*/
+
+int main()
+{
+    std::cout << "Hello World!\n";
+
+    system("PAUSE");
+    return 0;
+}
