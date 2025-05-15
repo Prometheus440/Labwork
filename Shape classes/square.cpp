ae90b2b280fc9c37da4241aa8bef260d928f6180
Matt Gilks <matthewgilks05@icloud.com>
2025-05-15 12:01:12 +0100

    Create square.cpp

---
+++ Shape classes/square.cpp
@@ -0,0 +1,11 @@
+//Define square class (inherits from shape class)
+
+#include "square.h"
+#include <iostream>
+
+using namespace std;
+
+int Square::calculateArea(int iHeightValue, int WidthValue)
+{
+	return 0;
+}
